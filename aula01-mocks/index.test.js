dae4856197f2fb77d87abcb562b94eecebcf7cde
Matheus Zago Bestana <matheusus1_23@hotmail.com>
2021-09-16 18:56:29 -0300

    Add files via upload

---
+++ aula01-mocks/index.test.js
@@ -0,0 +1,18 @@
+const { error } = require('./src/constants');
+const File = require('./src/file');
+const { rejects } = require('assert');
+
+(async() => {
+    {
+        const filePath = './mocks/emptyFile-invalid.csv';
+        const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+        const result = File.csvToJson(filePath);
+        await rejects(result, rejection);
+    }
+    {
+        const filePath = './mocks/fourItems-invalid.csv';
+        const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+        const result = File.csvToJson(filePath);
+        await rejects(result, rejection);
+    }
+})()
